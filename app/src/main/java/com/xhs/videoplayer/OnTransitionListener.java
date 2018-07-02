c4d19d3dc9d8fc757ef3334779f398c6f9af17cd
Android_Lzp <zp18595658325@163.com>
2018-07-02 18:16:25 +0800

    Initial commit

---
+++ app/src/main/java/com/xhs/videoplayer/OnTransitionListener.java
@@ -0,0 +1,41 @@
+package com.xhs.videoplayer;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.transition.Transition;
+
+
+/**
+ * Created by 布鲁斯.李 on 2018/7/2.
+ * Email:zp18595658325@163.com
+ */
+
+@TargetApi(Build.VERSION_CODES.KITKAT)
+public class OnTransitionListener implements Transition.TransitionListener {
+
+
+    @Override
+    public void onTransitionStart(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionEnd(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionCancel(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionPause(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionResume(Transition transition) {
+
+    }
+}
